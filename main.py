58f685f645e780b41ff193babf8965867ec8a13b
Deekshashandilya <30998084+Deekshashandilya@users.noreply.github.com>
2022-05-11 09:39:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import turtle
+from turtle import Turtle
+
+
+turt = Turtle()
+turt.color("red","red")
+turt.left(50)
+turt.fd(200)
+turt.left(24)
+turt.circle(72,167)
+turt.right(122)
+turt.circle(72,168)
+turt.left(24)
+turt.fd(212)
+
+print(turt)
+turtle.Screen().exitonclick()
+
+
+
+
+
